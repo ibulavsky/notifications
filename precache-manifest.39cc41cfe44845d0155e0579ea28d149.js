da207d84527882293b18fa19c5be526180194b4e
ibulavsky <ibulavsky@gmail.com>
2019-11-24 18:08:04 +0300

    Updates

---
+++ precache-manifest.39cc41cfe44845d0155e0579ea28d149.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24d78b37d56d837f911124a01f496df8",
+    "url": "/Authentification/index.html"
+  },
+  {
+    "revision": "d6fdb8c391dc0f1852b7",
+    "url": "/Authentification/static/css/main.7f9eb35d.chunk.css"
+  },
+  {
+    "revision": "cee6b15c12d3b098153e",
+    "url": "/Authentification/static/js/2.cebb52b4.chunk.js"
+  },
+  {
+    "revision": "d6fdb8c391dc0f1852b7",
+    "url": "/Authentification/static/js/main.9f9b5f4d.chunk.js"
+  },
+  {
+    "revision": "8520b8be4a4a4cb14904",
+    "url": "/Authentification/static/js/runtime-main.23bbeeeb.js"
+  }
+]);
